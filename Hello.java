0ebede9c7a894d9b94183c0aa5ee513b9408daeb
luokaisheng <2753734956@qq.com>
2020-06-14 21:11:26 +0800

    Commit Work Dir File

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
         //System.out.println("Hello, GIT!!!");
         System.out.println("I Love hhy!!!");
         System.out.println("hhy big fool!!!");
+        System.out.println("hhy big big super fool!!!");
     }
 }
