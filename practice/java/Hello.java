30fcb790399b3bb4046a9082f30a620388d82f4b
luokaisheng <2753734956@qq.com>
2020-06-17 19:26:44 +0800

    Change Hello.java In master Branch

--- practice/java/Hello.java
+++ practice/java/Hello.java
@@ -5,5 +5,7 @@ public class Hello{
         System.out.println("hhy big fool!!!");
         System.out.println("hhy big big super fool!!!");
         System.out.println(new java.util.Date());
+        Employee employee = new Employee();
+        System.out.println(employee);
     }
 }
