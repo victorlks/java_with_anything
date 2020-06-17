c3f5ce50f7f06ed0db9c06bc333923e63f1a6c6f
luokaisheng <2753734956@qq.com>
2020-06-17 19:22:23 +0800

    Change Hello.java in dev Branch

--- practice/java/Hello.java
+++ practice/java/Hello.java
@@ -1,9 +1,7 @@
+import java.util.*;
 public class Hello{
     public static void main(String[] args){
-        //System.out.println("Hello, GIT!!!");
-        System.out.println("I Love hhy!!!");
-        System.out.println("hhy big fool!!!");
-        System.out.println("hhy big big super fool!!!");
-        System.out.println(new java.util.Date());
+        System.out.println("Learn, Hungry");
+        Date date = new Date();
     }
 }
