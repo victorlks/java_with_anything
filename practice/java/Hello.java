484deee86507b8f03bb9fa10cb0417f63849ef02
luokaisheng <2753734956@qq.com>
2020-06-17 20:16:36 +0800

    Test Merge

--- practice/java/Hello.java
+++ practice/java/Hello.java
@@ -2,8 +2,6 @@ import java.util.*;
 public class Hello{
     public static void main(String[] args){
         //System.out.println("Hello, GIT!!!");
-        System.out.println("I Love hhy!!!");
-        System.out.println("hhy big fool!!!");
         System.out.println("hhy big big super fool!!!");
         System.out.println(new java.util.Date());
         Employee employee = new Employee();
